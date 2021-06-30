3cef299d9efdf5909613c6d48e712c244514430f
inkhorn <matt.dubins@donorscience.ca>
2021-06-30 11:37:59 -0400

    1) Altered the script to output Rmds so that it inserts a call to Michael's "do_chapter()" function inside an rmarkdown chunk.  Tested it and it works great! 2) Rmd files now draw figure info from TOGS_lof5.xlsx 3) I bugfixed the functions Michael created in chapter_figs.R
    Former-commit-id: 3f2fae7bc14d076b8bce00b6f742afa42fe13762

--- Output Distill Chapter RMDs.R
+++ Output Distill Chapter RMDs.R
@@ -6,7 +6,7 @@ library(rmarkdown)
 source("utils.R")
 
 togs = as.data.table(
-  openxlsx::read.xlsx("figureinfo/TOGS-lof4.xlsx")
+  openxlsx::read.xlsx("figureinfo/TOGS-lof5.xlsx")
 )
 
 distill_setup = readLines("Distill Page Setup.txt")
@@ -16,7 +16,6 @@ chap_rmds = list.files(path = "Synopsis RMDs", pattern = "ch.+\\.Rmd")
 for (f in chap_rmds) {
   
   chapnum = as.numeric(str_extract(f, "[0-9]+"))
-  togs_chap = togs[chapter == chapnum]
   
   in_rmd = readLines(str_c("Synopsis RMDs/",f))
   
@@ -24,19 +23,13 @@ for (f in chap_rmds) {
   
   rmd_mod[2] = glue(rmd_mod[2])
   
-  for (r in 1:nrow(togs_chap)) {
-    
-    newchunk = create_figure_chunk(togs_chap[r, filename],
-                                   togs_chap[r, title], togs_chap[r, subtitle],
-                                   togs_chap[r, fignum])
-    
-    rmd_mod = c(rmd_mod, "",
-                       str_split(newchunk, "\n")[[1]], "")
-  }
+  in_chunks = readLines("Chunk Template for Selected Figures.txt")
+  
+  in_chunks[5] = glue(in_chunks[5])
+  rmd_mod = c(rmd_mod, in_chunks)
   
   outrmd = file(glue("{f}"))
   
   writeLines(rmd_mod, outrmd)
 
-  #render(f)
 }
